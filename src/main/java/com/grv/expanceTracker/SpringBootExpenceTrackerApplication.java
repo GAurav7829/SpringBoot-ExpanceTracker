745a3291a9ddc505ee4dd86b7cc0f4da9dc349e1
Gaurav Singh <grvsgh7@gmail.com>
2022-03-23 21:11:37 +0530

    first commit

---
+++ src/main/java/com/grv/expanceTracker/SpringBootExpenceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.grv.expanceTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExpenceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExpenceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grv/expanceTracker/SpringBootExpenceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grv.expanceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExpenceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
